8986df1299263e5ea636f856ce29b85d253f54b7
Rick Dumpleton <dev.bites.uk@gmail.com>
2021-03-04 21:43:03 +0900

    add: login & cookies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import dotenv from 'dotenv';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import express from 'express';
 import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 import authRoutes from './routes/auth';
 
@@ -12,13 +16,13 @@ const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
-
+app.use(cookieParser());
 app.get('/', (_, res) => {
   res.send('Hello world');
 });
 app.use('/api/auth', authRoutes);
 
-app.listen(5000, async () => {
+app.listen(process.env.PORT, async () => {
   console.log(`Server running at localhost on 5000`);
 
   try {
