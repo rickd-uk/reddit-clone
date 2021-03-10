18ba1b1c9a1be967fd69793f12897b3125775a5c
Rick Dumpleton <dev.bites.uk@gmail.com>
2021-03-10 17:51:53 +0900

    add: register & login

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createConnection } from 'typeorm';
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -21,6 +22,13 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(trim);
 app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+    optionsSuccessStatus: 200,
+  }),
+);
 app.get('/', (_, res) => {
   res.send('Hello world');
 });
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,6 +9,12 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import cookie from 'cookie';
 
+const mapErrors = (errors: Object[]) => {
+  return errors.reduce((prev: any, err: any) => {
+    prev[err.property] = Object.entries(err.constraints)[0][1];
+    return prev;
+  }, {});
+};
 const register = async (req: Request, res: Response) => {
   const { email, username, password } = req.body;
 
@@ -22,7 +28,7 @@ const register = async (req: Request, res: Response) => {
     if (usernameUser) errors.username = `Username is already taken`;
 
     if (Object.keys(errors).length > 0) {
-      return res.status(400).json({ errors });
+      return res.status(400).json(errors);
     }
 
     // Create user
@@ -30,7 +36,9 @@ const register = async (req: Request, res: Response) => {
 
     errors = await validate(user);
 
-    if (errors.length > 0) return res.status(400).json({ errors });
+    if (errors.length > 0) {
+      return res.status(400).json(mapErrors(errors));
+    }
 
     await user.save();
 
@@ -53,10 +61,7 @@ const login = async (req: Request, res: Response) => {
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     const user = await User.findOne({ username });
-    if (!user)
-      return res
-        .status(404)
-        .json({ error: 'Username / Password is incorrect' });
+    if (!user) return res.status(404).json({ username: 'User not found' });
 
     const passwordMatches = await bcrypt.compare(password, user.password);
 
