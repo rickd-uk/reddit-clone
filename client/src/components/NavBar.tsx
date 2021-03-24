0ce0c15df4805c83aa03dc9183bffd53ccb5bcd7
Rick Dumpleton <dev.bites.uk@gmail.com>
2021-03-24 10:17:30 +0900

    add: search subs

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link';
-import { Fragment } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { useAuthState, useAuthDispatch } from '../context/auth';
 
 import RedditLogo from '../images/reddit.svg';
 import Axios from 'axios';
+import Sub from '../../../src/entities/Sub';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const NavBar: React.FC = () => {
+  const [name, setName] = useState('');
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
   const { authenticated, loading } = useAuthState();
 
+  const router = useRouter();
   const dispatch = useAuthDispatch();
 
   const logout = () => {
@@ -17,6 +24,35 @@ const NavBar: React.FC = () => {
         window.location.reload();
       })
       .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (name.trim() === '') {
+      setSubs([]);
+      return;
+    }
+    searchSubs();
+  }, [name]);
+
+  const searchSubs = async () => {
+    clearTimeout(timer);
+
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await Axios.get(`/subs/search/${name}`);
+          setSubs(data);
+          console.log(data);
+        } catch (err) {
+          console.log(err);
+        }
+      }, 200),
+    );
+  };
+
+  const goToSub = async (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName('');
   };
 
   return (
@@ -34,13 +70,36 @@ const NavBar: React.FC = () => {
       </div>
 
       {/* Search Input */}
-      <div className='flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white'>
+      <div className='relative flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white'>
         <i className='pl-4 pr-3 text-gray-500 fas fa-search' />
         <input
           type='text'
           className='py-1 pr-3 bg-transparent rounded w-160 focus:outline-none'
           placeholder='search'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className='absolute left-0 right-0 bg-white'
+          style={{ top: '100%' }}>
+          {subs?.map((sub) => (
+            <div
+              className='flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200'
+              onClick={() => goToSub(sub.name)}>
+              <Image
+                src={sub.imageUrl}
+                className='rounded-full'
+                alt='sub'
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+              />
+              <div className='ml-4 text-sm'>
+                <p className='font-medium'>{sub.name}</p>
+                <p className='text-gray-600'>{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       {/* Auth Buttons */}
       <div className='flex'>
--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -5,6 +5,7 @@ import useSWR from 'swr';
 import Head from 'next/head';
 import Axios from 'axios';
 import { Post, Sub } from '../../../types';
+import { GetServerSideProps } from 'next';
 
 export default function submit() {
   const [title, setTitle] = useState('');
@@ -80,3 +81,16 @@ export default function submit() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error('Missing auth token cookie');
+
+    await Axios.get('/auth/me', { headers: { cookie } });
+
+    return { props: {} };
+  } catch (err) {
+    res.writeHead(307, { Location: '/login' }).end();
+  }
+};
